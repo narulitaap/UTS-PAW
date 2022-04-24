057e458e6bdf783850b0392984f45573609aaac3
narulitaap <narulita.arien@gmail.com>
2022-04-24 09:45:59 +0700

    Menambahkan form tambah

---
+++ form_tambah.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Tambah</title>
+</head>
+<body>
+    <h1>Form Tambah</h1>
+    <form method="POST" action="action.php">
+        <p>Nama : <input type="text" name="nama_arin"></p><br/>
+        <p>Email: <input type="text" name="email_arin"></p><br/>
+        <p>Alamat : <input type="text" name="alamat_arin"></p><br/>
+        <button type="submit">Kirim</button>
+    </form>
+    
+</body>
+</html>
